0738386c8d3f20ad8f8441269c7b9f19ad0ae5b2
mehmettgur <mehmetgur@etu.edu.tr>
2025-03-25 16:15:55 +0300

    InputPreprocessingAgent

--- orchestrator.py
+++
@@ -1,6 +0,0 @@
-class OrchestratorAgent:
-    def __init__(self, intent_agent, specialized_agents, followup_agent, analytics_agent):
-        self.intent_agent = intent_agent
-        self.specialized_agents = specialized_agents
-        self.followup_agent = followup_agent
-        self.analytics_agent = analytics_agent
